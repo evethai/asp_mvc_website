4886d79be225b37e4906c0c378b64e804e5d0256
evethai <117005361+evethai@users.noreply.github.com>
2024-02-28 20:24:20 +0700

    web layout and controller call api

---
+++ asp_mvc_website/asp_mvc_website/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using asp_mvc_website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace asp_mvc_website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _client;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:7021/api/");
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        { 
+            //get list artwork
+            //List<ArtworkModel> artworkList = new List<ArtworkModel>();
+            //HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Artwork/GetAll").Result;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    string data = response.Content.ReadAsStringAsync().Result;
+            //    artworkList = JsonConvert.DeserializeObject<List<ArtworkModel>>(data);
+            //}
+            //return View(artworkList);
+
+            //get a artwork
+            ArtworkModel artwork = new ArtworkModel();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "Artwork/GetById/1").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                artwork = JsonConvert.DeserializeObject<ArtworkModel>(data);
+            }
+            return View(artwork);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ asp_mvc_website/asp_mvc_website/Models/ArtworkModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace asp_mvc_website.Models
+{
+    public class ArtworkModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double? Price { get; set; }
+        public string Image { get; set; } = string.Empty;
+
+        [JsonProperty("ImageUrl")]
+        private List<string> ImageUrl { get; set; } = new List<string>();
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            // Take the first URL from the list and assign it to ImageUrl
+            if (ImageUrl != null && ImageUrl.Count > 0)
+            {
+                Image = ImageUrl[0];
+            }
+        }
+    }
+}
