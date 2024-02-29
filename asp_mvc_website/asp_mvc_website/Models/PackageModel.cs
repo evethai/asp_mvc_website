2bde8c6dd7b9235a2cb2188b75f1ce82e07e0d4c
BlackerDK <duykhanhz1000st@gmail.com>
2024-02-29 18:12:25 +0700

    Create Controller Package, Html and Poster

---
+++ asp_mvc_website/asp_mvc_website/Models/PackageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace asp_mvc_website.Models
+{
+    public class PackageModel
+    {
+        public string Name { get; set; }
+        public int? Quantity { get; set; }
+        public double? Price { get; set; }
+        public bool Status { get; set; }
+    }
+}
