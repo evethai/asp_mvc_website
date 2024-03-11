0d04a588ebba6478dab7aaf0197897745b089351
cuongphan <cuongpvpvc@gmail.com>
2024-03-11 09:59:32 +0700

    userNotification

---
+++ asp_mvc_website/asp_mvc_website/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_mvc_website.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_website/asp_mvc_website/Controllers/UserNotifcationController.cs
@@ -0,0 +1,30 @@
+﻿using asp_mvc_website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace asp_mvc_website.Controllers
+{
+    public class UserNotifcationController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _client;
+        public UserNotifcationController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:44357/api/");
+        }
+        public IActionResult _IndexCha(string id)
+        {
+            List<GetUsetNotification> userNoti = new List<GetUsetNotification>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "UserNotifcation/"+ id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                userNoti = JsonConvert.DeserializeObject<List<GetUsetNotification>>(data);
+            }
+
+            return View(userNoti.ToList());
+        }
+    }
+}
